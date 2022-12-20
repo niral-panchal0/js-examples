7e26eebb3775959f1466f140acf68a97f3b6e4e9
niral-panchal0 <121100014+niral-panchal0@users.noreply.github.com>
2022-12-20 17:09:46 +0530

    file-server appendFile

--- fs-append.js
+++ fs-append.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-//create an empty file named mynewfile2.txt:
+//create an empty file named hello.txt:
 fs.appendFile('hello.txt', 'hello my name is niral', function (err, file) {
   if (err) throw err;
   console.log('Saved!');
